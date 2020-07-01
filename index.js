13067d14e7741b1cf767f9c788452a540df4bf22
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-07-01 21:22:54 +0300

    testattu REST clienttiä

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type' : 'text/plain'})
-    res.end('Heippa maailma!')
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2020-01-10T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2020-01-10T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2020-01-10T19:20:14.298Z",
+        important: true
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello!</h1>')
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)    // json-muodossa id on merkkijono --> muunnos luvuksi
+    const note = notes.find(note => note.id === id)
+
+    if (note)
+        res.json(note)
+    else
+        res.status(404).end()
+})
+
+app.delete(`/api/notes/:id`, (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+
