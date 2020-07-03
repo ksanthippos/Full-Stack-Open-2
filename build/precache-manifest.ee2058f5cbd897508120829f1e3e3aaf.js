0ac3161bbf134ca3b05a2618433dacf66b37c83d
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-07-03 08:55:41 +0300

    lisätty frontendin build, herokun testaus

---
+++ build/precache-manifest.ee2058f5cbd897508120829f1e3e3aaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d31f1f889e1bcfb13e1c5f20e24a60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21ef56a0b973b17edce8",
+    "url": "/static/css/main.ec822881.chunk.css"
+  },
+  {
+    "revision": "1040057a8355a3c67f30",
+    "url": "/static/js/2.d3cd1f96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3cd1f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ef56a0b973b17edce8",
+    "url": "/static/js/main.73cc9c74.chunk.js"
+  },
+  {
+    "revision": "b2a560344f9153bc0727",
+    "url": "/static/js/runtime-main.bf6f1ef8.js"
+  }
+]);
