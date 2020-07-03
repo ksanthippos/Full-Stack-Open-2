14b8db3c80c5711248e1242128842a912d4e7beb
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-07-03 09:45:36 +0300

    3.10 backend herokuun testaus

--- index.js
+++ index.js
@@ -1,84 +1,130 @@
 const express = require('express')
-const app = express()
-
+const morgan = require('morgan')
 const cors = require('cors')
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+// middlewaret
+app.use(express.json())
+//app.use(morgan('tiny'))
+// logataan myös data
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
-
+// apufunktio
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+// data
+let persons = [
+        {
+            "name": "Arto Hellas",
+            "number": "040-123456",
+            "id": 1
+        },
+        {
+            "name": "Ada Lovelace",
+            "number": "39-44-5323523",
+            "id": 2
+        },
+        {
+            "name": "Dan Abramov",
+            "number": "12-43-234345",
+            "id": 3
+        },
+        {
+            "name": "Mary Poppendieck",
+            "number": "39-23-6423122",
+            "id": 4
+        }
+]
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
+// GET
+app.get('/', (req, res) => {
+    res.send('<h1>Phonebook root</h1>')
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.get('/info', (req, res) => {
+
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+
+    res.send('<h1>Phonebook info</h1>' +
+        'Phonebook has info for ' + maxId + ' people <p/>' +
+        new Date()
+        )
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.get('/api/persons', (req, res) => {
+    if (persons)
+        res.json(persons)
+    else
+        res.status(404).end()
 })
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)    // json-muodossa id on merkkijono --> muunnos luvuksi
+    const person = persons.find(person => person.id === id)
+
+    if (person)
+        res.json(person)
+    else
+        res.status(404).end()
+})
+
+
+// DELETE
+app.delete(`/api/persons/:id`, (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+
+// POST
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    // tyhjä nimi tai numero --> ei hyväksytä
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    // tarkistetaan löytyykö samoja nimiä --> ei hyväksytä
+    const found = persons.find(person => person.name === body.name)
+
+    if (found) {
+        return res.status(400).json({
+            error: 'duplicate name found!'
+        })
+    }
+
+    // lisääminen ok
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
+
 
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
+
--- osa_3/3.7-3.8/index.js
+++ osa_3/3.7-3.8/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
-
 const app = express()
+
 
 // middlewaret
 app.use(express.json())
