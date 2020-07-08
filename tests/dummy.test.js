9bbb4f89ee92f74a76d8469d6de4a0395eecd5fe
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-07-08 09:56:04 +0300

    4.5 valmis

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -7,3 +7,4 @@ test('dummy returns one', () => {
 
 })
 
+
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,30 @@
-// eslint-disable-next-line no-unused-vars
+// testejä varten
+
 const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+
+  const blog = blogs.reduce(function(prev, current) {
+    return (prev.likes > current.likes) ? prev : current
+  })
+
+  const title = blog.title
+  const author = blog.author
+  const likes = blog.likes
+
+  return { title: title, author: author, likes: likes }
+
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
