4a5e248804d590fa0c29e3dd8ff9e3f0ada139f2
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-07-11 17:50:56 +0300

    aloitettu 4.8

--- utils/config.js
+++ utils/config.js
@@ -3,4 +3,7 @@ require('dotenv').config()
 let PORT = process.env.BLOG_PORT
 let MONGODB_URI = process.env.BLOG_URI
 
+if (process.env.NODE_ENV === 'test')
+    MONGODB_URI = process.env.TEST_BLOG_URI
+
 module.exports = { MONGODB_URI, PORT }
