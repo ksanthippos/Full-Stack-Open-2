8124548ef32058cbb526ac5ddccf7c3bd798b39e
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-07-15 11:15:51 +0300

    4.16 valmis

--- tests/user-api.test.js
+++ tests/user-api.test.js
@@ -6,11 +6,11 @@ const api = supertest(app)
 
 const User = require('../models/user')
 
+describe('initially one user at db', () => {
 
-describe('when there is initially one user at db', () => {
     beforeEach(async () => {
         await User.deleteMany({})
-        const user = new User({ username: 'root', password: 'sekret' })
+        const user = new User({ username: 'root', password: 'secret' })
         await user.save()
     })
 
@@ -18,8 +18,8 @@ describe('when there is initially one user at db', () => {
         const usersAtStart = await helper.usersInDb()
 
         const newUser = {
-            username: 'mluukkai',
-            name: 'Matti Luukkainen',
+            username: 'testi',
+            name: 'Testi Taneli',
             password: 'salainen',
         }
 
@@ -36,12 +36,12 @@ describe('when there is initially one user at db', () => {
         expect(usernames).toContain(newUser.username)
     })
 
-    test('creation fails with proper statuscode and message if username already taken', async () => {
+    test('creation fails with if username already taken', async () => {
         const usersAtStart = await helper.usersInDb()
 
         const newUser = {
             username: 'root',
-            name: 'Superuser',
+            name: 'Toinen Testaaja',
             password: 'salainen',
         }
 
@@ -52,6 +52,26 @@ describe('when there is initially one user at db', () => {
             .expect('Content-Type', /application\/json/)
 
         expect(result.body.error).toContain('`username` to be unique')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd.length).toBe(usersAtStart.length)
+    })
+
+    test('creation fails if username null', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            name: 'Kolmas Testaaja',
+            password: 'salainen',
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('`username` is required')
 
         const usersAtEnd = await helper.usersInDb()
         expect(usersAtEnd.length).toBe(usersAtStart.length)
