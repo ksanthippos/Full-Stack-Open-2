4e228ea415540677f19d1e7cb62fe0ce4234cd35
ksanthippos <mikael.rauhala@eduespoo.fi>
2020-07-15 18:38:01 +0300

    4.17 valmis

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,26 +1,35 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-
+const User = require('../models/user')
 
 // GET ALL
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
-  res.json(blogs.map(blog => blog.toJSON()))
+  const blogs = await Blog
+      .find({})
+      .populate('user', { username: 1, name: 1 })
+
+  await res.json(blogs.map(blog => blog.toJSON()))
 })
 
 // POST
 blogsRouter.post('/', async (req, res) => {
   const body = req.body
 
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
-  res.json(savedBlog.toJSON())
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  await res.json(savedBlog.toJSON())
 })
 
 // DELETE
@@ -38,8 +47,7 @@ blogsRouter.put('/:id', async (req, res) => {
   }
 
   const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {new: true, })
-  res.json(updatedBlog.toJSON())
-
+  await res.json(updatedBlog.toJSON())
 })
 
 module.exports = blogsRouter
